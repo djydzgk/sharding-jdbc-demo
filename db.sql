ece6cade13851d0cf4567b9e89152ac98515a1d1
zgk <zhang.gaoke@tslsmart.com>
2019-01-06 15:18:55 +0800

    copy from https://www.jianshu.com/p/3b2ab87b0de7 ,the db script of this project

---
+++ db.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE `user_0` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+CREATE TABLE `user_info_1` (
+  `user_id` bigint(19) NOT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `account` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `user_info_0` (
+  `user_id` bigint(19) NOT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `account` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE DATABASE `user_1` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+CREATE TABLE `user_info_1` (
+  `user_id` bigint(19) NOT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `account` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `user_info_0` (
+  `user_id` bigint(19) NOT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `account` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+作者：1994_老叶
+链接：https://www.jianshu.com/p/3b2ab87b0de7
+來源：简书
+简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
